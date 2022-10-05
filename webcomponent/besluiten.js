320ae4ab806bfe8ab7d96ef300943e3af5cb05a2
Pauwels Annelies <Annelies.Pauwels@district09.gent>
2022-10-05 17:33:27 +0200

    adding template and slots

--- webcomponent/besluiten.js
+++ webcomponent/besluiten.js
@@ -1,18 +1,13 @@
 const URL_GENT = "http://srvprobepr01.gentgrp.gent.be/sparql";
 const URL_LBLOD = "https://centrale-vindplaats.lblod.info/sparql";
 const URL_LBLOD_QA = "https://qa.centrale-vindplaats.lblod.info/sparql";
-const URL = URL_LBLOD_QA;
+const SPARQL_ENDPOINT = URL_GENT;
 
-const REGLEMENTEN = `
-PREFIX eli: <http://data.europa.eu/eli/ontology#>
-PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
-
-
-SELECT ?besluit ?title ?date WHERE {
-  ?besluit a besluit:Besluit ;
-    eli:date_publication ?date ;
-    eli:title_short ?title .
-} ORDER BY DESC(?date) LIMIT 5`;
+const BESTUURSEENHEID_URI = "";
+const BESTUURSORGANEN_URIS = "";
+const REGLEMENT_TYPES = "";
+const TITLE = "De meest recente reglementen";
+const BUTTON_URL = "";
 
 class Besluiten extends HTMLElement {
 
@@ -26,19 +21,53 @@ class Besluiten extends HTMLElement {
   }
 
   renderResults(reglementen) {
-    let html = '';
+    const template = document.getElementById("besluiten-template").content;
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(template.cloneNode(true));
+
+    // set parent slots
+    if (TITLE.length > 0) {
+      let slots = this.shadowRoot.querySelectorAll('slot');
+      slots.forEach(slot => {
+        switch (slot.name) {
+          case "list_title":
+            slot.outerHTML = TITLE;
+            break;
+        }
+      });
+    }
+
+    // create an element template for each query result element
     reglementen.forEach(reglement => {
-      html += `<p>reglement ${reglement.date.value}<br><strong>${reglement.title.value}</strong></p>`;
+      const elementTemplate = document.getElementById("besluit-element").content;
+      shadowRoot.appendChild(elementTemplate.cloneNode(true));
+
+      // set child slots
+      let slots = this.shadowRoot.querySelectorAll('slot');
+      slots.forEach(slot => {
+        console.log(slot);
+        switch (slot.name) {
+          case "item_title":
+            slot.outerHTML = reglement.title.value;
+            break;
+          case "item_time":
+            const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+            let date = new Date(reglement.date.value);
+            slot.outerHTML = date.toLocaleDateString('be-NL', options);
+            break;
+        }
+      });
     });
-    this.innerHTML = html;
+
   }
 
   async getReglementen() {
-    const endpoint = URL + "?query=" + encodeURIComponent(REGLEMENTEN);
-    const response = await fetch(endpoint,  
-      { headers: { 
-          'Content-Type' : 'application/x-www-form-urlencoded',
-          'Accept' : 'application/sparql-results+json'
+    const endpoint = SPARQL_ENDPOINT + "?query=" + encodeURIComponent(this.constructQuery());
+    const response = await fetch(endpoint,
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Accept': 'application/sparql-results+json'
         }
       });
 
@@ -51,6 +80,17 @@ class Besluiten extends HTMLElement {
     }
   }
 
+  constructQuery() {
+    return `PREFIX eli: <http://data.europa.eu/eli/ontology#>
+    PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
+    
+    
+    SELECT ?besluit ?title ?date WHERE {
+      ?besluit a besluit:Besluit ;
+        eli:date_publication ?date ;
+        eli:title_short ?title .
+    } ORDER BY DESC(?date) LIMIT 5`
+  }
 }
 
-customElements.define( 'besluiten-widget', Besluiten )
+customElements.define('besluiten-widget', Besluiten);
